b3544306e88b77cd7f6cf27d8f919f400a2f750f
Kristofer <kristofer.skyttner@skymaker.se>
2017-06-28 10:28:25 +0200

    Updated call structure
    Added container
    Removed mathjs dependency
    Replaced direct call with solve(a,b,c,d,e)

--- test/index.js
+++ test/index.js
@@ -1,19 +1,34 @@
-var chai = require('chai');
-chai.should();
+var expect = require('chai').expect;
 
-var complex = require('mathjs').complex;
 
-var solveQuartic = require('../index.js');
+var QUARTIC = require('../index.js');
+
+var complex = QUARTIC.ComplexNumber;
+
+var solveQuartic = QUARTIC.solve;
+
 
 describe('Quartic solver', function() {
   it('should find the correct roots of the equation', function() {
-    var coeffs = [5, 4, 3, 2, 1];
-
-    var roots = solveQuartic(coeffs);
-
-    roots[0].round(6).equals(complex( 0.137832,  0.678154)).should.be.true;
-    roots[1].round(6).equals(complex( 0.137832, -0.678154)).should.be.true;
-    roots[2].round(6).equals(complex(-0.537832,  0.358285)).should.be.true;
-    roots[3].round(6).equals(complex(-0.537832, -0.358285)).should.be.true;
+    
+    var roots = solveQuartic(5,4,3,2,1);
+    
+    expect(roots[0].getRounded(6)).to.deep.equal(new complex( 0.137832,  0.678154));
+    expect(roots[1].getRounded(6)).to.deep.equal(new complex(  0.137832, -0.678154));
+    expect(roots[2].getRounded(6)).to.deep.equal(new complex( -0.537832,  0.358285));
+    expect(roots[3].getRounded(6)).to.deep.equal(new complex( -0.537832, -0.358285));
   });
+
+
+  it('should find the correct roots of mixed real and imaginary', function() {
+
+    var roots = solveQuartic(-5,2,0,100,-65);
+    
+    expect(roots[0].getRounded(5)).to.deep.equal(new complex( 0.65354,  0));
+    expect(roots[1].getRounded(5)).to.deep.equal(new complex( -1.43069, 2.36237));
+    expect(roots[2].getRounded(5)).to.deep.equal(new complex( -1.43069,  -2.36237));
+    expect(roots[3].getRounded(5)).to.deep.equal(new complex(  2.60783, 0));
+    
+  });
+
 });
